dfe6cc087a44493ccb1f149808a27a4db5c209fa
micha.brzezicky <michal.brzezicky@seznam.cz>
2023-12-26 18:04:26 +0100

    add constraints and user anotations column add constructors

--- backend/src/main/java/uhk/project/webcontacts/backend/model/User.java
+++ backend/src/main/java/uhk/project/webcontacts/backend/model/User.java
@@ -2,24 +2,41 @@ package uhk.project.webcontacts.backend.model;
 
 import jakarta.persistence.*;
 
-@Entity
-@Table(name = "users")
+@Entity(name = "User")
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "user_email_unique", columnNames = "email")
+        }
+)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
     private Long id;
 
+    @Column(name = "name", nullable = false, columnDefinition = "TEXT")
     private String name;
+    @Column(name = "surname", nullable = false, columnDefinition = "TEXT")
     private String surname;
+    @Column(name = "email", nullable = false, columnDefinition = "TEXT")
     private String email;
+    @Column(name = "password", nullable = false, columnDefinition = "TEXT")
     private String password;
+
+    public User(String name, String surname, String email, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
