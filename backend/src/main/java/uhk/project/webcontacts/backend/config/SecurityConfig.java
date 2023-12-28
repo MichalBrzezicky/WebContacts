41969c087eec68b3930a0a2c279a630b1c0f17bc
micha.brzezicky <michal.brzezicky@seznam.cz>
2023-12-28 19:50:01 +0100

    - solve some details

--- backend/src/main/java/uhk/project/webcontacts/backend/config/SecurityConfig.java
+++ backend/src/main/java/uhk/project/webcontacts/backend/config/SecurityConfig.java
@@ -6,9 +6,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,23 +13,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable).cors(Customizer.withDefaults()).authorizeRequests(authorizeRequests -> {})
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeRequests(auth -> {
                     auth.requestMatchers("/").permitAll();
                     auth.anyRequest().authenticated();
-                }).httpBasic();
+                }).httpBasic(Customizer.withDefaults());
 
         return http.build();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User
-                .withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
     }
 }
--- backend/src/main/java/uhk/project/webcontacts/backend/repository/UserRepository.java
+++ backend/src/main/java/uhk/project/webcontacts/backend/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package uhk.project.webcontacts.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import uhk.project.webcontacts.backend.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
