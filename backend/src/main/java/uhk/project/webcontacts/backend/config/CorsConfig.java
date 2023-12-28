e1cc8602ee64fe1a58568d6c232cc2dbee58a906
micha.brzezicky <michal.brzezicky@seznam.cz>
2023-12-28 22:22:43 +0100

    - add semicolon to import

--- backend/src/main/java/uhk/project/webcontacts/backend/config/CorsConfig.java
+++ backend/src/main/java/uhk/project/webcontacts/backend/config/CorsConfig.java
@@ -3,7 +3,7 @@ package uhk.project.webcontacts.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class CorsConfig {
